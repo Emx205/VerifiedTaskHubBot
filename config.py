361e2750bfee145a9509446f9212d483b2bdf1a1
Emx205 <kpeesky84@gmail.com>
2026-07-24 15:20:13 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# ==========================
+# Verified Task Hub Bot Config
+# ==========================
+
+TOKEN = "8601184831:AAG7ktCJw4A-QOVZlKtec98S3i7Ua8DVxL8"
+
+ADMIN_ID = 8774157788
+
+ADMIN_USERNAME = "@Kpeesky"
+
+TASK_NAME = "HiFami Referral Task"
+
+TASK_REWARD = 200
+
+REFERRAL_LINK = "https://s.hifamiapp.com/1/4bvv4fhxp"
+
+BOT_NAME = "Verified Task Hub"
+
+SUPPORT = "@Kpeesky"
